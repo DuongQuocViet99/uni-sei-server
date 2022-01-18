76882b9d8cccc68153023d80560590b34331b31f
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-01-18 22:47:04 +0700

    use routes user

--- routers.js
+++ routers.js
@@ -1,5 +1,7 @@
 const project = require('./src/project/routes');
+const user = require('./src/user/routes');
 
 module.exports = app => {
   app.use('/project', project);
+  app.use('/user', user);
 };

8b5c64cc62e7307b89f30cb50f20c39c5e9f3fce
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-01-19 23:14:02 +0700

    get one project and create route

--- src/project/routes.js
+++ src/project/routes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const route = express.Router();
 
 const getAllproject = require('./views/get.all');
+const getOneproject = require('./views/get.one');
 
 route.get('/', getAllproject);
+route.get('/:projectid', getOneproject);
 
 module.exports = route;

9a1ff0d6c5f93ab3774df2411d9bddcdb255a1b5
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-01-24 18:01:45 +0700

    create route get one user

--- src/user/routes.js
+++ src/user/routes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const route = express.Router();
 
 const getAlluser = require('./views/get.all');
+const getOneuser = require('./views/get.one');
 
 route.get('/', getAlluser);
+route.get('/:projectid', getOneuser);
 
 module.exports = route;

59524a12063fdfb8526825e3f1898f66fa27cb7d
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-01-24 17:58:32 +0700

    use

--- src/user/views/get.all.js
+++ src/user/views/get.all.js
@@ -2,7 +2,7 @@ const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database('./db.sqlite3');
 
 module.exports = ( req, res ) => {
-  db.all(`SELECT * FROM user`, ( err, rows ) => {
+  db.all('SELECT * FROM user', ( err, rows ) => {
     res.json( rows );
   });
 }

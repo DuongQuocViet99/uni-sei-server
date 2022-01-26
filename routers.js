03cd32f155c1ee21bd419f3d6e3e4666db323135
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-01-26 18:58:07 +0700

    add router note

--- routers.js
+++ routers.js
@@ -1,9 +1,11 @@
 const project = require('./src/project/routes');
 const user = require('./src/user/routes');
 const task = require('./src/task/routes');
+const note = require('./src/note/routes');
 
 module.exports = app => {
   app.use('/project', project);
   app.use('/user', user);
   app.use('/task', task);
+  app.use('/note', note);
 };

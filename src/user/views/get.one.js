fab6c282993a6bd3325da9775bb553a58286bd6b
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-01-26 19:07:22 +0700

    add ;

--- src/user/views/get.one.js
+++ src/user/views/get.one.js
@@ -2,7 +2,7 @@ const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database('./db.sqlite3');
 
 module.exports = ( req, res ) => {
-  const { projectid } = req.params
+  const { projectid } = req.params;
   db.all(`SELECT * FROM user WHERE projectid=${ projectid }`, ( err, rows ) => {
     res.json( rows );
   });

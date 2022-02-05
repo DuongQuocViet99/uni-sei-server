87537405611587658b94974c85286c3ab05b5ead
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-02-05 19:13:04 +0700

    update: pass argument express

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
-require('./configs')( app );
+require('./configs')( app, express );
 require('./routers')( app );
 
 app.listen(port, () => {

33d5c4a85436fd4d2aae41af3afd7c61ee746814
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-02-05 19:07:45 +0700

    use urlencoded to use req.body

--- configs.js
+++ configs.js
@@ -1,5 +1,7 @@
 const cors = require('cors');
 
-module.exports = app => {
+module.exports = ( app, express ) => {
   app.use(cors());
+  app.use( express.urlencoded({ extended: true }) );
+  app.use( express.json() ); // To parse the incoming requests with JSON payloads
 }

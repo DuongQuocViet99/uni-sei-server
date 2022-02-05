e2ec1f5fa20f60aafa48172b67a5b6db8593771a
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-02-05 18:53:53 +0700

    route for putRenameproject

--- src/project/routes.js
+++ src/project/routes.js
@@ -3,8 +3,10 @@ const route = express.Router();
 
 const getAllproject = require('./views/get.all');
 const getOneproject = require('./views/get.one');
+const putRenameproject = require('./views/put.rename');
 
 route.get('/', getAllproject);
 route.get('/:projectid', getOneproject);
+route.put('/:projectid/rename', putRenameproject);
 
 module.exports = route;

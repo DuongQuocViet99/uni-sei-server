e9d98ad01e41aaad16e7b0a0c353c23bba6accd9
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-02-06 15:36:33 +0700

    add route put anchor

--- src/project/routes.js
+++ src/project/routes.js
@@ -4,9 +4,11 @@ const route = express.Router();
 const getAllproject = require('./views/get.all');
 const getOneproject = require('./views/get.one');
 const putRenameproject = require('./views/put.rename');
+const putAnchorproject = require('./views/put.anchor')
 
 route.get('/', getAllproject);
 route.get('/:projectid', getOneproject);
 route.put('/:projectid/rename', putRenameproject);
+route.put('/:projectid/anchor', putAnchorproject);
 
 module.exports = route;

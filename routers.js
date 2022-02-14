0906da4bbdd75f7dc430cccd12ae80f7a865f7eb
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-02-14 15:53:13 +0700

    add route action and process

--- routers.js
+++ routers.js
@@ -1,10 +1,14 @@
 const project = require('./src/project/routes');
+const process = require('./src/process/routes');
 const user = require('./src/user/routes');
 const task = require('./src/task/routes');
 const note = require('./src/note/routes');
+const action = require('./src/action/routes');
 
 module.exports = app => {
   app.use('/project', project);
+  app.use('/process', process);
+  app.use('/action', action);
   app.use('/user', user);
   app.use('/task', task);
   app.use('/note', note);

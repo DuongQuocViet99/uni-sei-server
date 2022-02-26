143389467b48bf6041aaa2f3afb952855a7b56ed
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-02-26 07:27:30 +0700

    delete one project

--- src/project/views/delete.one.js
+++ src/project/views/delete.one.js
@@ -3,7 +3,6 @@ const db = new sqlite3.Database('./db.sqlite3');
 
 module.exports = ( req, res ) => {
   const { projectid } = req.params;
-  const { anchor } = req.body;
-  db.all(`UPDATE project SET anchor='${ anchor }' WHERE projectid=${ projectid }`);
+  db.all(`DELETE FROM project WHERE projectid='${ projectid }'`);
   res.end();
 }

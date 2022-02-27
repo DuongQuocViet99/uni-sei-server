849d87906aa7dce66132c769cce85cdd7dd44105
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-02-27 12:09:03 +0700

    fix: delete table fk when delete parent table

--- src/project/views/delete.one.js
+++ src/project/views/delete.one.js
@@ -3,6 +3,7 @@ const db = new sqlite3.Database('./db.sqlite3');
 
 module.exports = ( req, res ) => {
   const { projectid } = req.params;
-  db.all(`DELETE FROM project WHERE projectid='${ projectid }'`);
+  db.all('PRAGMA foreign_keys = ON');
+  db.all(`DELETE FROM project WHERE projectid = ${ projectid };`);
   res.end();
 }

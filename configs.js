db8977218edbae81cf58422470cf94c6d6b72943
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-03-26 00:09:38 +0700

    use dotenv

--- configs.js
+++ configs.js
@@ -1,6 +1,7 @@
 const cors = require('cors');
 
 module.exports = ( app, express ) => {
+  require('dotenv').config();
   app.use(cors());
   app.use( express.urlencoded({ extended: true }) );
   app.use( express.json() ); // To parse the incoming requests with JSON payloads

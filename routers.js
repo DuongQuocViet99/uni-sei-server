b2c5ae31c1fea8090fc0b1dea0b2d3a7ad1049d8
Duong Quoc Viet <cp15.vietdq@gmail.com>
2022-03-26 09:34:14 +0700

    update

--- routers.js
+++ routers.js
@@ -2,7 +2,6 @@ const project = require('./src/project/routes');
 const process = require('./src/process/routes');
 const user = require('./src/user/routes');
 const task = require('./src/task/routes');
-const note = require('./src/note/routes');
 const action = require('./src/action/routes');
 const authentication = require('./src/authentication/routes');
 
@@ -13,5 +12,4 @@ module.exports = app => {
   app.use('/action', action);
   app.use('/user', user);
   app.use('/task', task);
-  app.use('/note', note);
 };
